8010b3c70815867343946d720c31c1b325a56a59
Jake Mackie <jake@hiyield.co.uk>
2025-07-16 08:06:12 +0100

    feat: add basic sitemap seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://windswept.bot',
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [sitemap()],
 });
